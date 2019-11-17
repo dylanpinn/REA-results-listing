b6d04402084c71922d2e840a33149447e54bc867
Dylan Pinn <me@dylanpinn.com>
2019-11-18 00:14:53 +1100

    Add eslint-plugin-hooks

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
 import React from 'react';
 
 function App() {
+  const [results, setResults] = React.useState([]);
+  const [saved, setSaved] = React.useState([]);
+
+  console.log(results);
+  console.log(saved);
+
   return <div>React</div>;
 }
 
