e9d29bc97a6482d52e4a996ad64ed6460b0b66b3
Dylan Pinn <me@dylanpinn.com>
2019-11-18 00:29:59 +1100

    Load data from static file

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React from 'react';
 
+// Load static data from file.
+// This would be loaded through an API in a production app.
+import data from './data/data.json';
+
+console.log(data);
+
 function App() {
   const [results, setResults] = React.useState([]);
   const [saved, setSaved] = React.useState([]);
 
-  console.log(results);
-  console.log(saved);
+  React.useEffect(() => {
+    setResults(data.result);
+    setSaved(data.saved);
+  }, []);
 
   return <div>React</div>;
 }
