5633ac8e76608f3d362074e625b269226bd341b9
Dylan Pinn <me@dylanpinn.com>
2019-11-18 01:50:40 +1100

    Render the rest of the property data
    - Agency information and main image

--- src/__tests__/property.js
+++ src/__tests__/property.js
@@ -11,4 +11,30 @@ describe('Property component', () => {
     const price = getByText(property.price);
     expect(price).toBeInTheDocument();
   });
+
+  it('renders the agency logo', () => {
+    const property = propertyBuilder();
+    const { getByAltText } = render(<Property property={property} />);
+
+    const price = getByAltText('Property agency logo');
+    expect(price).toBeInTheDocument();
+  });
+
+  it('renders the agency branding colours', () => {
+    const property = propertyBuilder();
+    const { getByTestId } = render(<Property property={property} />);
+
+    const price = getByTestId('agency-wrapper');
+    expect(price).toHaveStyle(
+      `background-color: ${property.agency.brandingColors.primary}`
+    );
+  });
+
+  it('renders the main image', () => {
+    const property = propertyBuilder();
+    const { getByAltText } = render(<Property property={property} />);
+
+    const price = getByAltText('Property main image');
+    expect(price).toBeInTheDocument();
+  });
 });
--- src/property.js
+++ src/property.js
@@ -4,13 +4,29 @@ import PropTypes from 'prop-types';
 export default function Property({ property }) {
   return (
     <div>
+      <div
+        style={{ backgroundColor: property.agency.brandingColors.primary }}
+        data-testid="agency-wrapper"
+      >
+        <img src={property.agency.logo} alt="Property agency logo" />
+      </div>
       <div>{property.price}</div>
+      <div>
+        <img src={property.mainImage} alt="Property main image" />
+      </div>
     </div>
   );
 }
 
 Property.propTypes = {
   property: PropTypes.shape({
-    price: PropTypes.string
+    price: PropTypes.string.isRequired,
+    agency: PropTypes.shape({
+      logo: PropTypes.string.isRequired,
+      brandingColors: PropTypes.shape({
+        primary: PropTypes.string.isRequired
+      }).isRequired
+    }),
+    mainImage: PropTypes.string.isRequired
   }).isRequired
 };
